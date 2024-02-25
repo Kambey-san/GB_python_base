2b82bc939e4ade6262224980e4a283b92fae57fc
Kambey-san <96491850+Kambey-san@users.noreply.github.com>
2024-02-25 16:26:15 +0300

    Add files via upload

---
+++ 01_Class_work.py
@@ -0,0 +1,28 @@
+# a = 4
+# b = 6
+# print(a * b)
+# print(b % a)
+# print(b // a)
+
+# num = int(input('Enter a number 0-9: '))
+
+# while num <= 9:
+#     print(num)
+#     break
+
+# i = 0
+#
+# while True:
+#     i += 1
+#     if i >= 10:
+#         break
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+
+
+print(f'{100 / 43:.2f}')    # округление
+
+print(f'{4: < 5}')
+print(f'{4: > 5}')
+print(f'{4: ^ 5}')
